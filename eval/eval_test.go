1661beb4a95244e8907b18358c0d7c483cf5a1b1
Ibrahim Hassan <ibrahim.hassan-3@student.manchester.ac.uk>
2023-07-23 16:16:24 +0100

    Began Evaluation Stage Development

---
+++ eval/eval_test.go
@@ -0,0 +1,42 @@
+package eval
+
+import (
+	"cardboard/lexer"
+	"cardboard/object"
+	"cardboard/parser"
+	"testing"
+)
+
+func TestEvalIntegerExpression(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"5", 5},
+		{"10", 10}}
+	for _, tt := range tests {
+		evaluated := testEval(tt.input)
+		testIntegerObject(t, evaluated, tt.expected)
+	}
+}
+
+func testEval(input string) object.Object {
+	l := lexer.CreateLexer(input)
+	p := parser.CreateParser(l)
+	program := p.ParseCardBoard()
+	return Eval(program)
+}
+
+func testIntegerObject(t *testing.T, obj object.Object, expected int64) bool {
+	result, ok := obj.(*object.Integer)
+	if !ok {
+		t.Fatalf("object is not Integer. got=%T", obj)
+		return false
+	}
+	if result.Value != expected {
+		t.Fatalf("object has wrong value. got=%d, want=%d",
+			result.Value, expected)
+		return false
+	}
+	return true
+}
